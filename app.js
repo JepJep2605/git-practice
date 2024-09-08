ba5ac74da3e318e90d489694c122a97b61cee653
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-09-08 13:53:50 +0000

    initial commit

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ let isTrue = true;
 let isFalse = false;
 
 // Comparison operators
-let isEqual = (num1 === num2);       // false, because 10 is not equal to 20
-let isNotEqual = (num1 !== num2);    // true, because 10 is not equal to 20
-let isGreater = (num1 > num2);       // false, because 10 is not greater than 20
-let isLess = (num1 < num2);          // true, because 10 is less than 20
+let isEqual = (num1 === num2);  
+let isNotEqual = (num1 !== num2); 
+let isGreater = (num1 > num2);  
+let isLess = (num1 < num2); 
 
 // Logical operation using Booleans
-let result = isTrue && isNotEqual;   // true, because both are true
-let result2 = isTrue || isFalse;     // true, because one of them is true
+let result = isTrue && isNotEqual; 
+let result2 = isTrue || isFalse; 
 
 // Output the results
-console.log(isEqual);        // false
-console.log(isNotEqual);     // true
-console.log(isGreater);      // false
-console.log(isLess);         // true
-console.log(result);         // true
-console.log(result2);        // true
+console.log(isEqual);    
+console.log(isNotEqual);   
+console.log(isGreater);      
+console.log(isLess);       
+console.log(result);
+console.log(result2);
