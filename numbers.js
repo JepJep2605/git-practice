804bddbed724e76fa0792ac3a56c87a4b6f1a43f
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-09-09 12:22:01 +0000

    initial commit

---
+++ numbers.js
@@ -0,0 +1,26 @@
+var firstFavNum = 7;
+var secondFavNum = 3;
+
+// ■ Addition
+var additionResult = firstFavNum + secondFavNum;
+console.log("Addition:", additionResult);
+
+// ■ Subtraction
+var subtractionResult = firstFavNum - secondFavNum;
+console.log("Subtraction:", subtractionResult);
+
+// ■ Multiplication
+var multiplicationResult = firstFavNum * secondFavNum;
+console.log("Multiplication:", multiplicationResult);
+
+// ■ Division
+var divisionResult = firstFavNum / secondFavNum;
+console.log("Division:", divisionResult);
+
+// ■ Modulus (Remainder)
+var modulusResult = firstFavNum % secondFavNum;
+console.log("Modulus:", modulusResult);
+
+// ■ Exponentiation (Power)
+var exponentiationResult = firstFavNum ** secondFavNum;
+console.log("Exponentiation:", exponentiationResult);
