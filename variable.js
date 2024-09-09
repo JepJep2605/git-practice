a38d231907a2acfadeb6010bb4be82f21c528e81
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-09-09 12:28:03 +0000

    initial commit

---
+++ variable.js
@@ -0,0 +1,9 @@
+var name = "Jeffrey Ragil"; var whatDoYouWannaBecomeInYourLife = "programmer";
+var gender = "Male";
+var twitterHandle = "@Jeffrey";
+
+// Log variables to the console
+console.log("Name:", name);
+console.log("What do you want to become:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
