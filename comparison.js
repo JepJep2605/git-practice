70dacbf1b69d1074fae8243de71b9a28a2ee9fb5
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-09-10 03:52:01 +0000

    initial commit

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNumb = 7; 
+let secondFavNumb = 3;   
+
+// Greater than
+console.log(firstFavNumb > secondFavNumb); 
+
+// Less than
+console.log(firstFavNumb < secondFavNumb); 
+
+// Greater than or equal to
+console.log(firstFavNumb >= secondFavNumb); 
+
+// Less than or equal to
+console.log(firstFavNumb <= secondFavNumb);
+
+// Strict equality (===)
+console.log(firstFavNumb === secondFavNumb); 
+
+// Loose equality (==)
+console.log(firstFavNumb == secondFavNumb); 
+
+// Strict inequality (!==)
+console.log(firstFavNumb !== secondFavNumb);
+
+// Loose inequality (!=)
+console.log(firstFavNumb != secondFavNumb); 
+
+// AND (both conditions must be true)
+console.log(firstFavNumb > 0 && secondFavNumb > 0);
+
+// OR (one condition must be true)
+console.log(firstFavNumb > 10 || secondFavNumb > 0); 
+
+// NOT (negation)
+console.log(!(firstFavNumb > secondFavNumb));
