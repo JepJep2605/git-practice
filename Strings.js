8209c6b250e33081b11d939053a061f40ed81052
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-09-13 01:49:30 +0000

    initial commit

---
+++ Strings.js
@@ -0,0 +1,26 @@
+
+const favActorFirstName = 'Eddie'; 
+const favActorLastName = 'Garcia';  
+
+
+const fullName = `${favActorEddie} ${favActorGarcia}`;
+
+
+const uppercase = fullName.toLowerCase();
+
+
+const petName = 'fluffy';
+const lowercase = petName.charAt(0).toUpperCase() + petName.slice(1);
+
+
+const message = `My favorite actor is ${EddieGarcia.toUpperCase()}`;
+
+
+const fullMessage = `${message}. His best show is Silicon Valley`;
+
+console.log('Favorite Actor First Name:', favActorFirstName);
+console.log('Favorite Actor Last Name:', favActorLastName);
+console.log('Full Name:', fullName);
+console.log('Uppercase:', uppercase);
+console.log('Lowercase:', lowercase);
+console.log('Message:', fullMessage);
